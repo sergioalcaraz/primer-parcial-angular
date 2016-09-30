04d0aab35362f781e17461cb6128eee9c3cb05d0
Sergio Alcaraz <siac215@gmail.com>
2016-09-29 20:30:11 -0400

    Http o Https

--- app/js/services/contactos-services.js
+++ app/js/services/contactos-services.js
@@ -1,6 +1,7 @@
 // Servicio para realizar peticiones al rest-api
 contactosServices.factory('Contactos', ['$resource', '$location',
     function($resource, $location) {
+        // multiples protocolos
         return $resource($location.protocol() + '://163.172.218.124/pwf/rest/agenda/:id', {'id': '@id'}, {
             query: {
                 isArray: false
