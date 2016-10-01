cb05a41daadbc2b50e3176bdee09e2a722e82495
Sergio Alcaraz <siac215@gmail.com>
2016-10-01 16:48:33 -0400

    Agregado confirmacion de modal bootstrap al modificar

--- app/js/controllers/contactos-modificar-ctrl.js
+++ app/js/controllers/contactos-modificar-ctrl.js
@@ -1,6 +1,6 @@
 // Controlador para la modificacion de contactos
-contactosControllers.controller('ContactosModificarCtrl', ['$scope', '$location', '$routeParams', 'Contactos',
-    function($scope, $location, $routeParams, Contactos) {
+contactosControllers.controller('ContactosModificarCtrl', ['$scope', '$location', '$routeParams', '$uibModal', 'Contactos',
+    function($scope, $location, $routeParams, $uibModal, Contactos) {
         $scope.mostrar = false
         $scope.contacto = Contactos.get({
             id: $routeParams.id
@@ -8,15 +8,32 @@ contactosControllers.controller('ContactosModificarCtrl', ['$scope', '$location'
             $scope.mostrar = true
         })
         $scope.modificar = function() { // Funcion disparado al hacer submit los datos
-            if (confirm('¿Desea modificar los datos?')) {
-                Contactos.update($scope.contacto,
-                    function(respuesta) {
-                        $location.url('/')
-                    },
-                    function() {
-                        alert('Error')
-                    })
-            }
+            var modificarModal = $uibModal.open({
+                ariaLabelledBy: 'modal-title',
+                ariaDescribedBy: 'modal-body',
+                templateUrl: 'modificar-confirmacion-modal.html',
+                controller: 'ModalContactosModificarCtrl',
+                backdrop: 'static',
+                scope: $scope
+            })
+            modificarModal.result.then(function() {
+                Contactos.update($scope.contacto, function() {
+                    $location.url('/')
+                })
+            })
+        }
+    }
+])
+
+// Controlador del modal
+contactosControllers.controller('ModalContactosModificarCtrl', ['$scope', '$uibModalInstance',
+    function($scope, $uibModalInstance) {
+        $scope.ok = function() {
+            $uibModalInstance.close()
+        }
+
+        $scope.cancelar = function() {
+            $uibModalInstance.dismiss()
         }
     }
 ])
