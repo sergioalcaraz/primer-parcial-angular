27a88944fd5c737139437a9a0a79a23adce30242
Sergio Alcaraz <siac215@gmail.com>
2016-10-01 11:37:24 -0400

    Agregado modal bootstrap al eliminar

--- app/js/controllers/contactos-lista-ctrl.js
+++ app/js/controllers/contactos-lista-ctrl.js
@@ -1,17 +1,40 @@
 // Controlador para la vista principal
-contactosControllers.controller('ContactosListaCtrl', ['$scope', 'Contactos',
-    function($scope, Contactos) {
+contactosControllers.controller('ContactosListaCtrl', ['$scope', '$uibModal', 'Contactos',
+    function($scope, $uibModal, Contactos) {
         $scope.contactos = Contactos.query()
 
         $scope.eliminar = function(contactoId) {
+            $scope.contactoId = contactoId
+            var eliminarModal = $uibModal.open({
+                ariaLabelledBy: 'modal-title',
+                ariaDescribedBy: 'modal-body',
+                templateUrl: 'borrar-confirmacion-modal.html',
+                controller: 'ModalContactosEliminarCtrl',
+                backdrop: 'static',
+                scope: $scope
+            })
 
-            if (confirm('¿Desea eliminar el contacto?')) {
-                Contactos.delete({id: contactoId}, function() {
+            eliminarModal.result.then(function() {
+                Contactos.delete({
+                    id: $scope.contactoId
+                }, function() {
                     $scope.contactos = null
                     $scope.contactos = Contactos.query()
                 })
-            }
+            })
+        }
+    }
+])
 
+// Controlador del modal
+contactosControllers.controller('ModalContactosEliminarCtrl', ['$scope', '$uibModalInstance',
+    function($scope, $uibModalInstance) {
+        $scope.ok = function () {
+            $uibModalInstance.close()
+        }
+
+        $scope.cancelar = function () {
+            $uibModalInstance.dismiss()
         }
     }
 ])
